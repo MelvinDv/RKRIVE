e0f4ce485908c11624f57711d19e2573b3db7db4
Melvin <melvindv01@gmail.com>
2022-11-30 16:14:46 -0700

    Se actualizo los css y primeras pruebas de API

--- controlador/canciones.js
+++ controlador/canciones.js
@@ -1,24 +1,33 @@
-var input = document.forms["formulario"]["buscador"].value;
-validateForm();
+$(document).ready(function(){
+  var getSong = function() {
 
-function validateForm() {
-  if (input == "") {
-    console.log("No se ha buscado nada");
-  } else {
-    var song = "dynamite";
+    var song = $("#buscador").val();
+    if (song == '') {
+      console.log("No se ha buscado nada");
+    } else {
 
-    const settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://k-pop.p.rapidapi.com/songs?q=" + song,
-      "method": "GET",
-      "headers": {
-        "X-RapidAPI-Key": "ff15588093mshf2c1a0bf9884472p1741f0jsnd0eebfebaa74",
-        "X-RapidAPI-Host": "k-pop.p.rapidapi.com"
-      }
-    };
-    $.ajax(settings).done(function (response) {
-      console.log(response);
-    });
+      console.log(song);
+  /*
+      const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://k-pop.p.rapidapi.com/songs?q=" + song,
+        "method": "GET",
+        "headers": {
+          "X-RapidAPI-Key": "ff15588093mshf2c1a0bf9884472p1741f0jsnd0eebfebaa74",
+          "X-RapidAPI-Host": "k-pop.p.rapidapi.com"
+        }
+      };
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+      });*/
+    }
   }
-}
+  $("#buscar").click(getSong);
+  $("#buscador").keyup(function(event){
+    if (event.keyCode == 13) {
+      getSong();
+    }
+  })
+
+});
