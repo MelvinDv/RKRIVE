13d3d2b7e1c894eab7e75bfd48c90ee6636c51f4
Fernando Ruelas <fernandor.ruelas@gmail.com>
2022-12-01 11:58:37 -0800

    Se agrego funcionalidad de buscar cancion

--- controlador/canciones.js
+++ controlador/canciones.js
@@ -1,13 +1,20 @@
+
+
 $(document).ready(function(){
+
+  /* ----- funcion de optener cancion -----*/
   var getSong = function() {
 
     var song = $("#buscador").val();
+
+    //validamos que no este basia la busqueda
     if (song == '') {
-      console.log("No se ha buscado nada");
+
+      $("#anuncio").toggleClass("invisible",false);
+      $("#anuncio").html("<h1>No se encontro ningun resultado</h1>")
+
     } else {
 
-      console.log(song);
-  /*
       const settings = {
         "async": true,
         "crossDomain": true,
@@ -20,14 +27,40 @@ $(document).ready(function(){
       };
       $.ajax(settings).done(function (response) {
         console.log(response);
-      });*/
+        
+        if (response.count =! 0) {
+          
+          $("#anuncio").toggleClass("invisible", false);
+          $("#anuncio").html("<h1>Esto fue lo que encontramos</h1>")
+
+          $.each(response.data, function(indice, contenido){
+            console.log(contenido);
+            $(".wrap_result").append('<ul><li><h1>'+contenido["Song Name"]+'</h1><h1>'+contenido["Korean Name"]+'</h1><h2>Artista: '+contenido.Artist+'</h2><p>Director: '+contenido.Director+'</p><small>Fecha: '+contenido.Date+'</small><video src="'+contenido.Video+'" width="100px" height="100px" poster="'+contenido.Video+'" autoplay="true"></video></li></ul>');
+          });
+        
+        }
+
+      
+
+
+      });
+
+      
     }
   }
+
+  /* ------Gatillos------*/
+
+  //gatillos para buscar 
   $("#buscar").click(getSong);
   $("#buscador").keyup(function(event){
     if (event.keyCode == 13) {
       getSong();
     }
   })
+
+
+
+
 
 });
